7ea191d7d508a257ce95fbfaf8c4d3570b57178f
Enes Yilmaz <enesyilmaz60@hotmail.com>
2020-11-23 17:10:34 +0100

    action dispatch reducer

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import UserConsumer from "../context"
 
 
 class User extends Component {
@@ -20,32 +21,41 @@ class User extends Component {
     })
   }
 
-  onDeleteUser = (e)=> {
-    // const {id} = this.props;
+  onDeleteUser = (dispatch,e)=> {
+    const {id} = this.props;
     // consumer dispatch
+    dispatch ({type : "DELETE_USER", payload:id})
   }
   render() {
 
     // Destructing
     const {name,department, salary,place} = this.props;
     const {isVisible} = this.state;
-    return (
-      <div className="col-md-8 mb-4">
-        <div className="card">
-          <div className = "card-header d-flex justify-content-between">
-            <h4 className = "d-inline" onClick={this.onClickEvent}>{name}</h4>
-            <i onClick={this.onDeleteUser} className="far fa-trash-alt" style={{cursor : "pointer"}}></i>
-          </div>
-          { isVisible ?
-          <div className = "card-body">
-            <p className="card-text">Gehalt : {salary}</p>
-            <p className="card-text">Abteilung : {department}</p>
-            <p className="card-text">Ort : {place}</p>
-          </div> : null
-          }
-        </div>
-      </div>
-    )
+    return (<UserConsumer>
+      {
+        value => {
+          const {dispatch} = value;
+          return (
+            <div className="col-md-8 mb-4">
+              <div className="card">
+                <div className = "card-header d-flex justify-content-between">
+                  <h4 className = "d-inline" onClick={this.onClickEvent}>{name}</h4>
+                  <i onClick={this.onDeleteUser.bind(this,dispatch)} className="far fa-trash-alt" style={{cursor : "pointer"}}></i>
+                </div>
+                { isVisible ?
+                <div className = "card-body">
+                  <p className="card-text">Gehalt : {salary}</p>
+                  <p className="card-text">Abteilung : {department}</p>
+                  <p className="card-text">Ort : {place}</p>
+                </div> : null
+                }
+              </div>
+            </div>
+          )
+        }
+      }
+    </UserConsumer>)
+
   }
 }
 User.propTypes = {
