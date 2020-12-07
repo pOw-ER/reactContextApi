eb6ede3a4f0aa0341430806f5110411fc61dbbf8
Enes Yilmaz <enesyilmaz60@hotmail.com>
2020-12-07 19:53:21 +0100

    get request with axios

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from "axios"
 
 const UserContext = React.createContext(); // provider, consumer
 
@@ -26,6 +27,12 @@ export class UserProvider extends Component {
     dispatch : action => {
       this.setState(state => reducer(state,action))
     }
+  }
+  componentDidMount = async() => {
+    const response =await axios.get("http://localhost:3004/users")
+    this.setState({
+      users : response.data
+    })
   }
   render() {
     return (
