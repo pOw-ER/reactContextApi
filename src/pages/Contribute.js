98288c6c3ae6e7db09d6ea63448ae6dcf8939a75
Enes Yilmaz <enesyilmaz60@hotmail.com>
2020-12-07 21:16:42 +0100

    refactoring

--- src/pages/Contribute.js
+++ src/pages/Contribute.js
@@ -5,7 +5,7 @@ function Contribute() {
     <div>
       <h3>My Project Files</h3>
       <p>
-        Du kannst meine alle Projekte unter <a href="https://github.com/pOw-ER">dieser Webseite</a> sehen und herunterladen. Du kannst auch dieses Projekt gerne beitragen.
+        Du kannst meine alle Projekte <a href="https://github.com/pOw-ER">unter dieser Webseite</a> sehen und herunterladen. Du kannst auch dieses Projekt gerne beitragen.
       </p>
     </div>
   )
